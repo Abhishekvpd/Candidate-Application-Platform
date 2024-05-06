91552771cea1ae9b44b2a56f8206ea871736f781
Abhishek <abhishek.shettigar@coditas.com>
2024-05-06 13:33:37 +0530

    added infinite scroll

--- src/pages/searchJobs/SearchJobs.jsx
+++ src/pages/searchJobs/SearchJobs.jsx
@@ -2,18 +2,36 @@ import "./searchJobs.css";
 import Box from "@mui/material/Box";
 import { CircularProgress, Grid } from "@mui/material";
 import JobCard from "../../components/jobCard/JobCard";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { processJobs, updatePageOffset } from "../../store/features/jobSlice";
 
 const SearchJobs = () => {
     const dispatch = useDispatch();
     const jobState = useSelector(state => state.jobs);
-    const { jds, status, pageOffset, pageSize } = jobState;
+    const { jds: { list }, status, pageOffset, pageSize } = jobState;
+    const loadingRef = useRef(null);
 
     useEffect(() => {
         dispatch(processJobs(pageOffset));
     }, [pageOffset])
+
+    useEffect(() => {
+        //intersection observer to check the loading div is intersecting the viewport
+        const loadingObserver = new IntersectionObserver(([target]) => {
+            if (target.isIntersecting) dispatch(updatePageOffset({ value: (pageOffset + pageSize) }))
+        }, { threshold: 1 });
+
+        //observe if ref is present and list length exists
+        if (loadingRef.current && list.length) {
+            loadingObserver.observe(loadingRef.current);
+        }
+
+        //clean up the current observer on component unmount
+        return () => {
+            if (loadingRef.current) loadingObserver.unobserve(loadingRef.current);
+        }
+    }, [list])
 
     return (
         <div className="page__container">
@@ -23,17 +41,17 @@ const SearchJobs = () => {
 
             <main>
                 {
-                    status === "pending" && !jds.totalCount ? < CircularProgress /> :
-                        <Grid container spacing={{ xs: 3 }} columns={{ xs: 1, sm: 2, md: 3 }} paddingTop={2.5}>
-                            {jds?.list.map(job => (
-                                <Grid item key={job.jdUid} xs={1} sm={1} md={1}>
-                                    <JobCard jobDetails={job} />
-                                </Grid>
-                            ))}
-                        </Grid>
+                    list.length > 0 &&
+                    (<Grid container spacing={{ xs: 3 }} columns={{ xs: 1, sm: 2, md: 3 }} paddingTop={2.5}>
+                        {list.map(job => (
+                            <Grid item key={job.jdUid} xs={1} sm={1} md={1}>
+                                <JobCard jobDetails={job} />
+                            </Grid>
+                        ))}
+                    </Grid>)
                 }
+                <div ref={loadingRef} className="loader">{status === "pending" && < CircularProgress />}</div>
             </main>
-            <button onClick={() => (dispatch(updatePageOffset({ value: (pageOffset + pageSize) })))}>Load</button>
         </div>
     )
 }
