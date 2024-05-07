f3c9cb39eef488c62e00e8dddc9287abc97ce7b6
Abhishek <abhishek.shettigar@coditas.com>
2024-05-07 07:39:08 +0530

    added see more functionality for viewing job description

--- src/components/jobCard/JobCard.jsx
+++ src/components/jobCard/JobCard.jsx
@@ -1,13 +1,16 @@
-import { Box, Button, Card, CardContent, Paper } from "@mui/material";
+import { Box, Button, Card, CardContent, Paper, Typography } from "@mui/material";
 import "./jobCard.css";
+import { useState } from "react";
 
 const JobCard = ({ jobDetails }) => {
-    const { logoUrl, companyName, jobRole, location, minJdSalary, maxJdSalary, salaryCurrencyCode, minExp, maxExp } = jobDetails;
+    const [isJobDescriptionExpanded, setIsJobDescriptionExpanded] = useState(false);
+
+    const { logoUrl, companyName, jobRole, location, minJdSalary, maxJdSalary, salaryCurrencyCode, minExp, maxExp, jobDetailsFromCompany } = jobDetails;
     const estimatedSalary = minJdSalary && maxJdSalary ? `${minJdSalary} - ${maxJdSalary}` : minJdSalary || maxJdSalary;
 
     return (
         <Card className="job-card" sx={{ maxWidth: 360, borderRadius: 5, boxShadow: 'rgba(0, 0, 0, 0.25) 0px 1px 4px 0px', padding: 1.5 }}>
-            <CardContent sx={{ width: '100%', padding: '0.5rem', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+            <CardContent sx={{ width: '100%', padding: '0.5rem', display: 'flex', flexDirection: 'column' }}>
                 <Box sx={{ display: 'flex', gap: '0.5rem' }}>
                     <Box component="img" src={logoUrl} alt="logo" sx={{ height: '2.5rem', width: '2.5rem' }}></Box>
                     <div className="card__header">
@@ -22,7 +25,14 @@ const JobCard = ({ jobDetails }) => {
                     (minJdSalary || maxJdSalary) &&
                     <p className="job-salary">Estimated Salary: {`${estimatedSalary} ${salaryCurrencyCode}`}</p>
                 }
-                <Box sx={{ height: '250px' }}></Box>
+                <Box sx={{ height: '250px', display: 'flex', flexDirection: 'column', ...(isJobDescriptionExpanded ? { overflowY: 'scroll' } : { overflow: 'hidden', maskImage: 'linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0))' }) }}>
+                    <Typography variant="p" sx={{ fontSize: '1rem', fontWeight: 500, lineHeight: 1.5 }}>Job Details:</Typography>
+                    <Typography variant="p" sx={{ fontSize: '14px', fontWeight: 400 }}>{jobDetailsFromCompany}</Typography>
+                </Box>
+
+                <Box sx={{ textAlign: 'center', position: 'relative', top: '-20px', ...(isJobDescriptionExpanded && { visibility: 'hidden' }) }}>
+                    <Button variant="text" sx={{ color: '#4943da', textTransform: 'none' }} onClick={() => setIsJobDescriptionExpanded(true)}>See more</Button>
+                </Box>
 
                 <div>
                     <h3>Minimum Experience</h3>
